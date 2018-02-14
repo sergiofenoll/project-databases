dd61acb32576667832bc65d5331f967f6e76b314
DeSchepperCedric <cedric.deschepper99@gmail.com>
2018-02-14 13:50:00 +0100

    basic creation of user sql table

---
+++ user.sql
@@ -0,0 +1,12 @@
+CREATE TABLE User (
+  Username VARCHAR(255),
+  Password VARCHAR(255) NOT NULL,
+  FirstName VARCHAR(255),
+  LastName VARCHAR(255),
+  Email VARCHAR(255),
+  Status BIT NOT NULL,
+  Active BIT NOT NULL,
+  
+  PRIMARY KEY(Username),
+  CHECK Status IN ("user","admin")
+)

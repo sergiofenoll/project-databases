df8a58f8d92e88fe4749e0580264087cfaaeb342
Sergio Fenoll <sergio.fenoll@gmail.com>
2018-02-15 20:34:45 +0100

    Added basic login and register forms Updated Flask to load the forms. The login form will check against a dictionary of users (this will later be replaced by an access to the database). If login fails, the page is reloaded and a link is added to the register form (register form currently does nothing).

--- project-databases.py
+++ project-databases.py
@@ -1,10 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request, session, jsonify
+from passlib.hash import sha256_crypt
 app = Flask(__name__)
 
 
+# Mock users
+mock_users = {'sff': sha256_crypt.encrypt('password')}
+
+
+# API
+@app.route('/login', methods=['POST'])
+def send_login_request():
+    username = request.form.get('lg-username')
+    password = request.form.get('lg-password')
+
+    print('Validating data for user "{0}"'.format(username))
+
+    if username not in mock_users or sha256_crypt.verify(password, mock_users[username]):
+        return render_template('login-form.html', failed_login=True)
+    return 'Logged in!'
+
+
+# Views
 @app.route('/')
 def index():
     return "Hello, World!"
+
+
+@app.route('/login')
+def login():
+    return render_template('login-form.html', failed_login=False)
+
+
+@app.route('/register')
+def register():
+    return render_template('register-form.html')
+
 
 if __name__ == "__main__":
     app.run()

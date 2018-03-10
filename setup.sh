8339ed751472ff97c8684b7e1885f5041e982935
Sergio Fenoll <sergio@fenoll.be>
2018-03-10 16:24:50 +0100

    Updated Travis-CI config file, removed .pyc files and added setup script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# If you're encountering this error: psql: FATAL: Peer authentication failed for user "postgres"
+# See this answer: https://stackoverflow.com/a/21166595 and change authentication method to trust for postgres and *all* users
+# If you're still not sure about what to do, send Sergio a message
+
+# If you don't use virtualenv the modules will be installed systemwide!
+# Please consider installing virtualenv (sudo apt install python3-virtualenv in Ubuntu)
+
+psql -U postgres -c "CREATE ROLE dbadmin WITH LOGIN PASSWORD 'dbadmin';"
+psql -U postgres -c "ALTER ROLE dbadmin CREATEDB;"
+
+psql -U postgres -c "CREATE DATABASE userdb OWNER dbadmin;"
+psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE userdb TO dbadmin;"
+
+psql -U dbadmin -d userdb -f sql/tables.sql
+
+cp src/default_config.py src/config.py
+
+virtualenv venv/
+source venv/bin/activate
+pip3 install -r requirements.txt
+deactivate

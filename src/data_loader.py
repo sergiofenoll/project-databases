e1608178ed431dc225068cab73ddaa8c462aa059
De Schepper Cedric <cedric.deschepper99@gmail.com>
2018-03-12 17:42:43 +0100

    nec commit

--- src/data_loader.py
+++ src/data_loader.py
@@ -502,7 +502,6 @@ class DataLoader:
         """
          This method returns a list of column names associated with the given table
         """
-
         cursor = self.dbconnect.get_cursor()
 
         try:

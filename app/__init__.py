e78404995284685f4dff4f82c135c4a9c9048e85
De Schepper Cedric <cedric.deschepper99@gmail.com>
2018-04-10 18:02:42 +0200

    fix undo error

--- app/__init__.py
+++ app/__init__.py
@@ -15,12 +15,14 @@ app.config.from_object('config')  # See: http://flask.pocoo.org/docs/0.12/config
 from app.data_service.models import DataLoader
 from app.database_connection.models import DBConnection
 from app.user_service.models import UserDataAccess
+from app.data_transform.models import DateTimeTransformer
 
 try:
     connection = DBConnection(dbname=config_data['dbname'], dbuser=config_data['dbuser'], dbpass=config_data['dbpass'],
                               dbhost=config_data['dbhost'])
     user_data_access = UserDataAccess(connection)
     data_loader = DataLoader(connection)
+    date_time_transformer = DateTimeTransformer(connection)
 except Exception as e:
     app.logger.error("[ERROR] Failed to establish user connection.")
     app.logger.exception(e)

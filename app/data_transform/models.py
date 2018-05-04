ba8b93d02a36bee9b145ce8876fc295d1791ce3e
De Schepper Cedric <cedric.deschepper99@gmail.com>
2018-05-04 12:17:15 +0200

    add fill by mcv

--- app/data_transform/models.py
+++ app/data_transform/models.py
@@ -73,14 +73,35 @@ class DataTransformer:
             app.logger.exception(e)
             raise e
 
+    def impute_missing_data_on_mcv(self, schema_id, table, column):
+        """" impute missing data based on the average"""
+        try:
+            schema_name = 'schema-' + str(schema_id)
+
+            dl = DataLoader()
+            value = dl.calculate_most_common_value(schema_id, table, column)
+
+            db.engine.execute('UPDATE {0}.{1} SET {2} = {3} WHERE {2} IS NULL;'.format(*_ci(schema_name, table, column),
+                                                                                       _cv(value)))
+            history.log_action(schema_id, table, datetime.now(), 'Imputed missing data on median')
+
+        except Exception as e:
+            app.logger.error("[ERROR] Unable to impute missing data for column {} by median".format(column))
+            app.logger.exception(e)
+            raise e
+
     def impute_missing_data(self, schema_id, table, column, function):
         """"impute missing data based on the average"""
         if function == "AVG":
             return self.impute_missing_data_on_average(schema_id, table, column)
         elif function == "MEDIAN":
             return self.impute_missing_data_on_median(schema_id, table, column)
+        elif function == "MCV":
+            print(column)
+            return self.impute_missing_data_on_mcv(schema_id, table, column)
         else:
             app.logger.error("[ERROR] Unable to impute missing data for column {}".format(column))
+            raise Exception
 
     def find_and_replace(self, schema_id, table, column, to_be_replaced, replacement, replacement_function):
         """" find and replace """

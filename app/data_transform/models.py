cbc73d723ec1b8dd863c7215a695f137d3764b55
DawidMiroyan <dawid_miroyan@hotmail.com>
2018-05-24 13:37:55 +0000

    Full find and replace queyr fix

--- app/data_transform/models.py
+++ app/data_transform/models.py
@@ -135,7 +135,7 @@ class DataTransformer:
                                                                                   *_cv(to_be_replaced, replacement))
             elif replacement_function == "full replace":
                 updated_rows = [row['id'] for row in db.engine.execute('SELECT id FROM {}.{} WHERE {}={}'.format(
-                    schema_name, table, column, replacement)).fetchall()]
+                    *_ci(schema_name, table, column), _cv(replacement))).fetchall()]
                 query = 'UPDATE {0}.{1} SET {2} = {3} WHERE {2} = {4};'.format(*_ci(schema_name, table, column),
                                                                                *_cv(replacement, to_be_replaced))
             else:
@@ -145,7 +145,7 @@ class DataTransformer:
             if replacement_function == 'substring':
                 inverse_query = ''
                 for row_id in updated_rows:
-                    inverse_query += 'UPDATE {0}.{1} SET {2} = REPLACE({2}, {3}, {4} WHERE id = {});'.format(
+                    inverse_query += 'UPDATE {0}.{1} SET {2} = REPLACE({2}, {3}, {4} WHERE id = {5});'.format(
                             *_ci(schema_name, table, column), *_cv(to_be_replaced, replacement), row_id)
             else:
                 inverse_query = ''

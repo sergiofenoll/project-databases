29abb9bcaf5b980e6959346047633d028923c5b5
De Schepper Cedric <cedric.deschepper99@gmail.com>
2018-05-25 16:32:21 +0200

    fix variable hiding

--- app/data_service/models.py
+++ app/data_service/models.py
@@ -536,7 +536,7 @@ class DataLoader:
             history.log_action(schema_id, table, datetime.now(), 'Added row with values ' + ' '.join(values),
                                inverse_query)
 
-    def insert_column(self, schema_id, table_name, column_name, column_type, history=True):
+    def insert_column(self, schema_id, table_name, column_name, column_type, enable_history=True):
         schema_name = 'schema-' + str(schema_id)
         try:
             db.engine.execute(
@@ -548,7 +548,7 @@ class DataLoader:
             raise e
 
         # Log action to history
-        if history:
+        if enable_history:
             inverse_query = 'ALTER TABLE {}.{} DROP COLUMN IF EXISTS {};'.format(*_ci(schema_name, table_name, column_name))
             history.log_action(schema_id, table_name, datetime.now(), 'Added column with name ' + column_name,
                                inverse_query)
